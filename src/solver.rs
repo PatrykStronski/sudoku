d59d64412364b1fa8ecc7f547581e3bc2b908297
Patryk Stronski <p.stronski13@gmail.com>
2020-05-03 23:21:37 +0200

    introduced time calculations

--- src/solver.rs
+++ src/solver.rs
@@ -1,4 +1,5 @@
 use crate::sudoku::Sudoku;
+use std::time::{Duration, Instant};
 
 fn backtrack_step(sdk: &mut Sudoku, mut pos_x: usize, mut pos_y: usize) -> bool {
     if pos_x == 9 && pos_y == 8 {
@@ -34,16 +35,20 @@ fn backtrack(sdk: &mut Sudoku) -> bool {
 
 pub fn solve_sudoku(sdk: &mut Sudoku) -> bool {
     println!("{}", sdk.print_current_board());
+    let now = Instant::now();
     if sdk.validate_solution() {
         if backtrack(sdk) {
             println!("{}", sdk.print_current_board());
             println!("Errors with solution: {}", sdk.get_errors_with_solution());
+            println!("Execution time: {}", now.elapsed().as_millis());
             return true;
         } else {
             println!("UNSOLVABLE");
+            println!("Execution time: {}", now.elapsed().as_millis());
             return false;
         }
     }
     println!("INVALID INPUT DATA");
+    println!("Execution time: {}", now.elapsed().as_millis());
     return false;
 }
