14fc23ad3cca7d1cfc17f4d3ad208d2f20897104
Patryk Stronski <p.stronski13@gmail.com>
2020-05-18 23:36:17 +0200

    removed brute force

--- src/solver.rs
+++ src/solver.rs
@@ -13,37 +13,12 @@ fn backtrack_step(sdk: &mut Sudoku, mut pos_x: usize, mut pos_y: usize) -> bool 
         pos_x = 0;
     }
     if !sdk.is_empty(pos_x, pos_y) {
-        return backtrack_forward_step(sdk, pos_x + 1, pos_y);
-    }
-    let possible_solutions = vec![1,2,3,4,5,6,7,8,9];
-    for sol in possible_solutions {
-        sdk.insert_field(pos_x, pos_y, sol);
-        if backtrack_step(sdk, pos_x + 1, pos_y) {
-            return true;
-        }
-        sdk.nullify_field(pos_x, pos_y);
-    }
-    return false;
-}
-
-fn backtrack_forward_step(sdk: &mut Sudoku, mut pos_x: usize, mut pos_y: usize) -> bool {
-    if pos_x == 9 && pos_y == 8 {
-        if sdk.validate_solution() {
-            return true;
-        }
-        return false;
-    }
-    if pos_x == 9 {
-        pos_y += 1;
-        pos_x = 0;
-    }
-    if !sdk.is_empty(pos_x, pos_y) {
-        return backtrack_forward_step(sdk, pos_x + 1, pos_y);
+        return backtrack_step(sdk, pos_x + 1, pos_y);
     }
     let possible_solutions = sdk.get_possible_solutions(pos_x, pos_y);
     for sol in possible_solutions {
         sdk.insert_field(pos_x, pos_y, sol);
-        if backtrack_forward_step(sdk, pos_x + 1, pos_y) {
+        if backtrack_step(sdk, pos_x + 1, pos_y) {
             return true;
         }
         sdk.nullify_field(pos_x, pos_y);
@@ -65,16 +40,7 @@ fn backtrack(sdk: &mut Sudoku) -> bool {
 }
 
 fn backtrack_forward_check(sdk: &mut Sudoku) -> bool {
-    let now = Instant::now();
-    if backtrack_forward_step(sdk, 0 as usize, 0 as usize) {
-        println!("{}", sdk.print_current_board());
-        println!("Errors with solution: {}", sdk.get_errors_with_solution());
-        println!("Execution time with backtrack w forward checking: {}", now.elapsed().as_millis());    
-        return true;
-    }
-    println!("UNSOLVABLE");
-    println!("Execution time: {}", now.elapsed().as_millis());
-    return false;
+    return true;
 }
 
 pub fn solve_sudoku(sdk: &mut Sudoku) -> bool {
